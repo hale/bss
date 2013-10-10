4f542dd6186b6a2541c4d84c80c9e936dce786a8
Philip Hale <phil989@gmail.com>
2013-10-10 18:18:15 +0100

    Better Tomdoc documentation

--- lib/bss.rb
+++ lib/bss.rb
@@ -1,8 +1,21 @@
+require_relative 'array_extensions'
+
 module BSS
 
-  # The swaps needed to transform source into target.
+  # Public: Index-pairs to transform source into target.
   #
-  # A swap is a pair of indices which when applied to source, 
+  # A swap is a pair of indices which when applied to an array results in a
+  # different ordering of that array.
+  #
+  # a - The source array
+  # b - The target array
+  #
+  # Examples
+  #
+  #  swaps([1,2,3], [2,3,1])
+  #  # => [[0,1],[1,2]]
+  #
+  # Returns an array of swaps.
   def self.swaps(a, b)
     source = a.dup
     target = b
@@ -18,6 +31,20 @@ module BSS
     end
   end
 
+  # Public: Swap the elements of the tuple at the given indices
+  #
+  # A swap is a pair of indices which when applied to an array results in a
+  # different ordering of that array.
+  #
+  # swap - An index pair
+  # tuple - An ordered set.
+  #
+  # Examples
+  #
+  #   apply!([0,3], [1,2,3])
+  #   # => [3,2,1]
+  #
+  # Returns the effects of tuple + swap.
   def self.apply!(swap, tuple)
     tuple.swap_indices!(*swap)
   end
